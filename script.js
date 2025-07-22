c82775e51d18299e322d67e7758fa08923135cc2
yunseounghwan0921 <nayo20120@gmail.com>
2025-07-22 20:41:01 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function login(event) {
   const password = document.getElementById('password').value;
   const error = document.getElementById('error-message');
 
-  if (username === 'nsb' && password === '25') {
+  if (username === 'Nsb' && password === '25') {
     window.location.href = 'https://yunseounghwan0921.github.io/main/'; // 성공 시 이동
   } else {
     error.textContent = '아이디 또는 비밀번호가 틀렸습니다.';
